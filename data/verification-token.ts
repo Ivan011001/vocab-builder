e886a7e07cd7a79a385594cf89829690940af8f8
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-03 18:30:08 +0200

    fix: verification token query

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -2,7 +2,7 @@ import { db } from "@/lib/db";
 
 export const getVerificationTokenByToken = async (token: string) => {
   try {
-    const verificationToken = await db.verificationToken.findFirst({
+    const verificationToken = await db.verificationToken.findUnique({
       where: {
         token,
       },
