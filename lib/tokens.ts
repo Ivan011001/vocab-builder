c8fda154416047a98d610d2b1957afc5e24f476e
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-04 20:04:49 +0200

    fix: 2fa token lifetime

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -9,7 +9,7 @@ import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100000, 1000000).toString();
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
   const existingToken = await getTwoFactorTokenByEmail(email);
 
