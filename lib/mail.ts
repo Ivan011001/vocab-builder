78f01964cb98952d75f6c0bf335f498e5eb4aebd
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-06 15:49:05 +0200

    fix: added env domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,6 +2,8 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -12,7 +14,7 @@ export const sendTwoFactorEmail = async (email: string, token: string) => {
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/new-verification?token=${token}`;
+  const confirmLink = `${domain}/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -23,7 +25,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetToken = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/new-password?token=${token}`;
+  const resetLink = `${domain}/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
