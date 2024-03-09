0ba28c10803bfd6065ccbf0561c68a355f75948f
georgijbudik <georgijbudik@gmail.com>
2024-03-09 17:33:07 +0100

    feat: create header component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./_components/navbar";
+import Header from "./_components/header";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex flex-col gap-y-2 justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <Navbar />
+    <div className="h-full flex flex-col gap-y-2 items-center">
+      <Header />
       {children}
     </div>
   );
