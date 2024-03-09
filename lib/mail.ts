dca4bf58d35550f9e77d8ac0355816dd8e3ca8b5
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-09 13:28:28 +0200

    feat: added custom email domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@gore-itishniki.online",
     to: email,
     subject: "2FA verification",
     html: `<p>Your 2FA code is ${token}</p>`,
@@ -17,7 +17,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@gore-itishniki.online",
     to: email,
     subject: "Confirm email",
     html: `<p>Click <a href=${confirmLink}>here</a> to confirm email</p>`,
@@ -28,7 +28,7 @@ export const sendPasswordResetToken = async (email: string, token: string) => {
   const resetLink = `${domain}/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@gore-itishniki.online",
     to: email,
     subject: "Reset password",
     html: `<p>Click <a href=${resetLink}>here</a> to reset password</p>`,
