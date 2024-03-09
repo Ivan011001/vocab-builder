571924d34b91bc2898d9ee9e6714080c544de5ea
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-09 21:13:27 +0200

    feat: created error page styles

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -6,11 +6,11 @@ const ErrorCard = () => {
   return (
     <CardWrapper
       backButtonHref="/login"
-      backButtonLabel="Back to login"
-      headerTitle="Ooops... Something went wrong"
+      backButtonLabel="Login"
+      headerTitle="Please try again!"
     >
       <div className="w-full flex justify-center">
-        <ExclamationTriangleIcon className="text-destructive h-8 w-8" />
+        <ExclamationTriangleIcon className="text-destructive h-10 w-10" />
       </div>
     </CardWrapper>
   );
