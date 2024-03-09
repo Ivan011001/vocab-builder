505ffa693ccd8d9dcf344cf7ee16a17e6273d8ab
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-09 21:23:08 +0200

    fix: reading image

--- app/(auth)/_components/auth-image.tsx
+++ app/(auth)/_components/auth-image.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 const AuthImage = () => {
   return (
     <Image
-      className="w-[247px] h-[191px] lg:h-[435px] lg:w-[498px]"
+      className="w-[247px] h-[191px] lg:h-[405px] lg:w-[498px]"
       width={498}
-      height={435}
+      height={405}
       alt="Reading book"
       src="/reading.png"
       priority
