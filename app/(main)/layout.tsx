19f555c4ce2b73dcf68d227671ec5c3d7c4f8ac7
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-10 13:13:52 +0200

    fix: main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,9 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex flex-col gap-y-2 items-center">
       <Header />
-      {children}
+      <div className="w-full px-4 md:px-8 pt-8 md:pt-[80px] pb-[48px] lg:px-[100px]">
+        {children}
+      </div>
     </div>
   );
 };
