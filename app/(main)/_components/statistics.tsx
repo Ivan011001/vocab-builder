34026001df42f2eea57d24b6aad141700d4aa560
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-10 13:13:38 +0200

    feat: created statistics

---
+++ app/(main)/_components/statistics.tsx
@@ -0,0 +1,12 @@
+const Statistics = () => {
+  return (
+    <div className="flex items-center gap-x-2">
+      <p className="text-neutral-900 text-opacity-50 text-sm md:text-base font-medium md:leading-normal">
+        To study:
+      </p>
+      <p className="text-neutral-900 text-xl font-medium">20</p>
+    </div>
+  );
+};
+
+export default Statistics;
