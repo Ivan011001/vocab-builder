e038aab4f0aa8559cb772d0ad2555fd234fd1f1c
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-11 20:45:46 +0200

    fix: categories select

--- app/(main)/_components/categories.tsx
+++ app/(main)/_components/categories.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import { Fragment, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
-import { Listbox, Transition } from "@headlessui/react";
-import { FaChevronDown } from "react-icons/fa";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import { getCategories } from "@/data/categories";
 
 import { ICategorie } from "@/types";
+
+import { capitalizeWord } from "@/helpers";
 
 const Categories = () => {
   const { replace } = useRouter();
@@ -16,9 +24,6 @@ const Categories = () => {
   const searchParams = useSearchParams();
 
   const [categories, setCategories] = useState<ICategorie[] | null>([]);
-  const [selected, setSelected] = useState({
-    name: searchParams.get("categorie")?.toString() || "Categories",
-  });
 
   useEffect(() => {
     const fetch = async () => {
@@ -30,88 +35,49 @@ const Categories = () => {
     fetch();
   }, []);
 
-  const onHandleChange = (data: ICategorie) => {
+  const onHandleChange = (data: string) => {
     const params = new URLSearchParams(searchParams);
 
-    const { name } = data;
-
-    if (name === "all") {
+    if (data === "all") {
       params.delete("categorie");
 
       replace(`${pathname}?${params.toString()}`);
 
-      setSelected(data);
-
       return;
     }
 
-    if (name !== "verb") {
+    if (data !== "verb") {
       params.delete("verbType");
     } else {
       params.set("verbType", "regular");
     }
 
-    if (name) {
-      params.set("categorie", name);
+    if (data) {
+      params.set("categorie", data);
     } else {
       params.delete("categorie");
     }
 
     replace(`${pathname}?${params.toString()}`);
-
-    setSelected(data);
   };
 
   return (
-    <div className="md:w-40">
-      <Listbox value={selected} onChange={onHandleChange}>
-        <div className="relative">
-          <Listbox.Button className="relative w-full flex items-center justify-between py-3 px-6 rounded-[15px] bg-transparent border border-neutral-900 border-opacity-10 text-neutral-900 text-base font-medium leading-normal focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300">
-            <span className="block truncate">{selected.name}</span>
+    <Select onValueChange={onHandleChange}>
+      <SelectTrigger className="md:w-40 rounded-[15px] px-6 py-3 border border-neutral-900 border-opacity-10 text-neutral-900 text-base font-medium leading-normal">
+        <SelectValue placeholder="Categories" />
+      </SelectTrigger>
+      <SelectContent className="md:w-40">
+        <SelectGroup className="flex flex-col gap-2 px-6 py-3">
+          {categories?.map((categorie) => (
+            <SelectItem value={categorie.name} key={categorie.id}>
+              {capitalizeWord(categorie.name)}
+            </SelectItem>
+          ))}
 
-            <FaChevronDown
-              className="h-5 w-5 text-neutral-800"
-              aria-hidden="true"
-            />
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute mt-1 w-full py-3 px-6 space-y-2 overflow-auto rounded-md bg-white text-base ring-1 ring-black/5 focus:outline-none">
-              {categories?.map((categorie) => (
-                <Listbox.Option
-                  key={categorie.id}
-                  className={({ active }) =>
-                    `relative select-none ${active ? "" : ""}`
-                  }
-                  value={categorie}
-                >
-                  {() => (
-                    <span className="block cursor-pointer truncate text-neutral-900 text-base font-medium leading-normal">
-                      {categorie.name}
-                    </span>
-                  )}
-                </Listbox.Option>
-              ))}
-
-              <Listbox.Option
-                className={({ active }) =>
-                  `relative select-none ${active ? "" : ""}`
-                }
-                value={{ name: "all" }}
-              >
-                <span className="block cursor-pointer truncate text-neutral-900 text-base font-medium leading-normal">
-                  all
-                </span>
-              </Listbox.Option>
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
-    </div>
+          <SelectItem value="all">All</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 };
 
--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -25,13 +25,13 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex w-full items-center justify-between whitespace-nowrap rounded-[15px] text-neutral-50 text-base  leading-normal border border-gray-300 bg-transparent ring-offset-background focus:outline-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex w-full items-center justify-between gap-x-3 whitespace-nowrap text-base  leading-normal border border-gray-300 bg-transparent ring-offset-background focus:outline-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
   >
     {children}
-    <FaChevronDown className="h-4 w-4 text-neutral-50" aria-hidden="true" />
+    <FaChevronDown className="h-4 w-4 flex-shrink-0" aria-hidden="true" />
   </SelectPrimitive.Trigger>
 ));
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
