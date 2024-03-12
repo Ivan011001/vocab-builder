d6c9ec7cfb30c36753932e0099b449f2c32dd2aa
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-12 17:53:30 +0200

    feat: added dictionary search

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -1,18 +1,65 @@
+import { Prisma } from "@prisma/client";
 import { db } from "./db";
 
-export const getUserDictionary = async (userId: string | null | undefined) => {
+export const getUserDictionary = async (
+  userId: string | null | undefined,
+  search: string,
+  category: string,
+  page: number
+) => {
   try {
     if (!userId) {
       return null;
     }
 
-    const dictionary = await db.word.findMany({
+    const skip = (page - 1) * 7;
+
+    const totalCount = await db.word.count({
       where: {
-        userId,
+        category: {
+          equals: category,
+          mode: "insensitive",
+        },
+        word: {
+          contains: search,
+          mode: "insensitive",
+        },
       },
     });
 
-    return dictionary;
+    const whereClause: Prisma.WordWhereInput = {
+      userId,
+      word: {
+        contains: search,
+        mode: "insensitive",
+      },
+    };
+
+    if (category !== "") {
+      whereClause.category = {
+        equals: category,
+        mode: "insensitive",
+      };
+    }
+
+    const dictionary = await db.word.findMany({
+      take: 7,
+      skip,
+      where: whereClause,
+    });
+
+    const hasPrevPage = page > 1;
+    const hasNextPage = skip + dictionary.length < totalCount;
+
+    return {
+      data: dictionary,
+      meta: {
+        totalCount,
+        currentPage: page,
+        hasPrevPage,
+        hasNextPage,
+      },
+    };
   } catch {
     return null;
   }
