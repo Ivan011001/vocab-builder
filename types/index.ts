1314ec61c9f9793a31bee7fef2da8252d020afbd
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-12 16:37:36 +0200

    feat: created types for table

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,18 @@ export interface ICategorie {
   id: string;
   name: string;
 }
+
+export interface IRecommend {
+  id: string;
+  word: string;
+  translation: string;
+  category: string;
+}
+
+export interface IDictionary {
+  id: string;
+  word: string;
+  translation: string;
+  category: string;
+  progress: string;
+}
