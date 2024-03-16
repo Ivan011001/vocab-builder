be82e6c57f34efee5ab40080803e59aa0d406ec5
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-16 21:27:37 +0200

    fix: user statistics

--- components/main/statistics.tsx
+++ components/main/statistics.tsx
@@ -1,10 +1,18 @@
-const Statistics = () => {
+import { currentUser } from "@/lib/auth";
+
+import { getUserWordsCount } from "@/data/words-count";
+
+const Statistics = async () => {
+  const user = await currentUser();
+
+  const wordsCount = await getUserWordsCount(user?.id!);
+
   return (
     <div className="flex items-center gap-x-2">
       <p className="text-neutral-900 text-opacity-50 text-sm md:text-base font-medium md:leading-normal">
         To study:
       </p>
-      <p className="text-neutral-900 text-xl font-medium">20</p>
+      <p className="text-neutral-900 text-xl font-medium">{wordsCount}</p>
     </div>
   );
 };
