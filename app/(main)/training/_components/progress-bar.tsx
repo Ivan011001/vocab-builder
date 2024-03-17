141738779cf4f446a98ab19686f65c1aefd17d48
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-03-17 13:31:50 +0200

    fix: training progress bar

--- app/(main)/training/_components/progress-bar.tsx
+++ app/(main)/training/_components/progress-bar.tsx
@@ -1,34 +1,19 @@
 "use client";
 
-import { useCurrentUser } from "@/hooks";
-import { useEffect, useState } from "react";
-
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
-import { getUserWordsCount } from "@/data/statistics";
+interface IProgressBarProps {
+  totalCount: number;
+  currentStreak: number;
+}
 
-const ProgressBar = () => {
-  const [wordsCount, setWordsCount] = useState<number | null>(null);
-  const user = useCurrentUser();
-
-  useEffect(() => {
-    const fetch = async () => {
-      const data = await getUserWordsCount(user?.id!);
-
-      setWordsCount(data);
-    };
-
-    fetch();
-  }, [user]);
-
-  const currentStrike = 1;
-
+const ProgressBar = ({ totalCount, currentStreak }: IProgressBarProps) => {
   return (
     <div className="w-full flex items-center justify-end">
       <div className="h-11 w-11 md:h-[58px] md:w-[58px] relative">
         <CircularProgressbar
-          value={currentStrike}
-          maxValue={wordsCount!}
+          value={currentStreak}
+          maxValue={totalCount!}
           strokeWidth={10}
           styles={buildStyles({
             pathTransitionDuration: 0.5,
@@ -41,7 +26,7 @@ const ProgressBar = () => {
         />
 
         <p className="absolute bottom-[50%] right-[50%] translate-y-[50%] translate-x-[50%] text-center text-neutral-900 text-base font-medium">
-          {currentStrike}
+          {currentStreak}
         </p>
       </div>
     </div>
